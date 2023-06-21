9c237ef28c0b198381a558ab90de390776509da3
MisakiSan3 <iot.gomez@yavirac.edu.ec>
2023-06-21 14:59:58 -0500

    servicio

--- src/modules/ventas/services/ventas.service.ts
+++ src/modules/ventas/services/ventas.service.ts
@@ -4,7 +4,8 @@ import { Inject } from '@nestjs/common/decorators';
 import { RepositoryEnum } from 'src/shared/enum';
 import { Product } from '../product/entities/product.entity';
 import { Repository } from "typeorm/repository/Repository";
-import { CreateProductDto, FitlerProductDto, ReadProductDto } from '../product/dto';
+import { CreateProductDto, FitlerProductDto, PaginationProductDto, ReadProductDto, UpdateProductDto } from '../product/dto';
+import { FindOptionsWhere, ILike } from 'typeorm';
 
 @Injectable()
 export class VentasService {
@@ -16,6 +17,7 @@ export class VentasService {
    const productCreated = await this.repository.save(newProduct);
     return {data: plainToInstance(ReadProductDto, productCreated)};
   }
+  
 
   async catalogue():Promise<ServiceResponseHttpModel>{
     const response = this.repository.findAndCount({take: 1000});
@@ -41,16 +43,51 @@ export class VentasService {
         return response;
       }
   }
-
-  findOne(id: number) {
-    return `This action returns a # product`;
+  async updateOne(id: string, data: UpdateProductDto):Promise<ServiceResponseHttpModel>{
+    const response = await this.repository.findOneBy({id})
+    if(!response) {
+      throw new NotFoundException('No se encuentra registros');
+      
+    }else{
+      this.repository.merge(await response,data)
+    }
+    return this.repository.save(response)
   }
 
-  update() {
-    return `This action updates a # product`;
+  async removeOne(id: string):Promise<ServiceResponseHttpModel>{
+    const response = await this.repository.findBy({id});
+    if(!response) {
+      throw new NotFoundException('No se encuentra registros');
+      
+    }else{
+      this.repository.softRemove(await response)
+    }
   }
-
-  remove(id: number) {
-    return `This action removes a # product`;
+  async removeAllListeners(data: Product[]):Promise<ServiceResponseHttpModel>{
+    const response = await this.repository.softRemove(data);
+  }
+  private async paginateAndFilter(params: FitlerProductDto){
+    let where:
+    FindOptionsWhere<Product>;
+    FindOptionsWhere<Product> [];
+      where = {};
+      let {page,search} = params;
+      const {limit} = params
+      if (search) {
+        search = search.trim(),
+        page= 0;
+        where = [];
+        where.push({name: ILike(`%${search}`)})
+       
+      }
+      const data = await this.repository.findAndCount({
+        relations: ['bloodtype', 'gender'],
+        where,
+        take:limit,
+        skip: PaginationProductDto.getOffset(limit,page),
+      });
+      return  {pagination: {limit, yotalItems: data[1]},data: [0]}
   }
 }
+
+○
